de94545d01acad858cc9bd7994d45d1fb4d9a4c0
Sidharth Dhawan <sidharthdhawan17@gmail.com>
2023-07-27 19:35:42 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const books = [
+    {
+        author: 'Kai Bard',
+        title: 'American Prometheus: The Triumph and Tragedy of J. Robert OppenheimerAmerican Prometheus: The Triumph and Tragedy of J. Robert Oppenheimer', 
+        img: 'https://images-na.ssl-images-amazon.com/images/I/81yfsIOijJL._AC_UL600_SR600,400_.jpg',
+        id: 1,
+    },
+    {
+        author: 'James Clear',
+        title: 'Atomic Habits', 
+        img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
+        id: 2,
+    },
+];
+
+// const  names = ['john','peter','susan'];
+// const newNames = names.map((name)=> {
+//     return <h1>{name}</h1>;
+// })                                           // iterating over the list
+
+const BookList = () => {
+    return <section className='booklist'>
+       {books.map((book)=> {
+        const {img, title, author, id} = book;
+        return <Book img = {img} title = {title} author = {author} key = {id} />;
+       })}
+    </section>;
+}
+
+const title = 'American Prometheus: The Triumph and Tragedy of J. Robert Oppenheimer' //more efficient practice
+const img = 'https://images-na.ssl-images-amazon.com/images/I/81yfsIOijJL._AC_UL600_SR600,400_.jpg'
+const author = 'Kai Bard'
+
+const Book = (props) => {
+    const {img, title, author} = props;
+    return (<article className='book'>
+        <img src={img} alt={title} />
+        <h2>{title}</h2>             
+        <h4>{author}</h4>
+    </article>);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList/>)
